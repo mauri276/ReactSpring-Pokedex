1dda1611d1d7c9a6d89d1913e663a3e0ed1cae20
Mauricio Pernia <mauriciopernia67@gmail.com>
2023-11-19 21:26:06 -0400

    Agregado Nombre, stats, imagen y tipo del pokemon

--- pokemon-app-frontend/src/components/Resultados.jsx
+++ pokemon-app-frontend/src/components/Resultados.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import Stats from './Stats.jsx';
 import '../stylesheets/components/Resultados.css';
 
 function Resultados ( props ) {
     
     return (
         <>
-            <div className="pantalla-pokedex_container">
-                <div className="pokemon-img_container">
-                    {/* <img src={props.pokemonData.imagenOficial} className="pokemon_img" alt="Img del Pokemon" /> */}
+            <div className="col-pantalla-type">
+                <div className="pantalla-pokedex_container">
+                    {props.pokemonData ? <h2 className="pokemon_name">{props.pokemonData.name}</h2> : null}
+                    <div className="pokemon-information_container">
+                        <div className="pokemon-img_container">
+                            {props.pokemonData ? <img src={props.pokemonData.imagenOficial} className="pokemon_img" alt="Img del Pokemon" /> : null}
+                        </div>
+                        <div className="stats_container">
+                            {props.pokemonData ? <Stats pokemonData = { props.pokemonData }/> : null}
+                        </div>
+                    </div>
+                </div>
+                <div className="type_container">
+                    <textarea name="" id="" readOnly className="pokemon-type_textarea" value={props.pokemonData ? props.pokemonData.types[0].type.name : "Type"}/>
                 </div>
             </div>
-            <p>
-                {/* Descripción: {props.pokemonData.flavor_text} */}
-            </p>
+            <div className="description_container">
+                <textarea name="" id="" readOnly className="pokemon-description_textarea" value={props.pokemonData ? props.pokemonData.flavor_text : "Description"}/>
+            </div>
         </>
     );
 }
